7d47e12d4413bde55e12e6962f15171e3fef6d1c
github username <github email address>
2019-10-22 19:21:11 -0400

    testing arrown funciton with curly bracket

--- src/components/currentPosition.js
+++ src/components/currentPosition.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 
 
 const CurrentPositon = ({ text }) => (
-  <div style={{
-    color: 'yellow',
-    background: 'blue',
-    padding: '15px 10px',
-    display: 'inline-flex',
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: '100%',
-    transform: 'translate(-50%, -50%)',
+    <div style={{
+      color: 'yellow',
+      background: 'blue',
+      padding: '15px 10px',
+      display: 'inline-flex',
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: '100%',
+      transform: 'translate(-50%, -50%)',
 
-  }}>
+    }}>
     {text}
-  </div>
+    </div>
 );
 
 export default CurrentPositon;

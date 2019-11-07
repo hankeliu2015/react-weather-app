95869018e62aafe7587e14804ace95a8c476e542
github username <github email address>
2019-11-07 11:35:49 -0500

    import and apply style to the component render return content

--- src/components/currentGeo.js
+++ src/components/currentGeo.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 
-const Title = styled.h3`
-font-size: 1.5 em;
-text-align: center;
-color: palevioletred;
-`;
-const Wrapper = styled.section`
-padding; 4em;
-background: papayawhip;
-`;
+// const Title = styled.h3`
+//   font-size: 1.5 em;
+//   text-align: center;
+//   color: palevioletred;
+// `
+// const Wrapper = styled.section`
+//   padding; 4em;
+//   background: papayawhip;
+// `
+
+const Wrapper = styled.div`
+  background: grey;
+  color: white;
+`
 
 class CurrentGeo extends Component {
 
   render() {
     return (
-      <div>
+      <Wrapper>
         <h3>Current GEOLocaiton data from props</h3>
         <p>latitude: {this.props.latitude}</p>
         <p>longitude: {this.props.longitude}</p>
-      </div>
+      </Wrapper>
     )
   }
 }

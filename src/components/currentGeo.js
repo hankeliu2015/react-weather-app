304df9cbeb77632f93b3d8deaf05701f38f1cd92
github username <github email address>
2019-11-06 23:00:07 -0500

    testing wrapper and title component

--- src/components/currentGeo.js
+++ src/components/currentGeo.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
+const Title = styled.h3`
+font-size: 1.5 em;
+text-align: center;
+color: palevioletred;
+`;
+const Wrapper = styled.section`
+padding; 4em;
+background: papayawhip;
+`;
+
 class CurrentGeo extends Component {
+
+
+
   render() {
     return (
       <div>

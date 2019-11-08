c1a863a92626538eef9502e634caf6e267c9a63f
github username <github email address>
2019-11-08 16:04:35 -0500

    create styleTest2.js component to test apply style to components

---
+++ src/components/styleTest2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+
+class StyleTest2 extends Component {
+  render() {
+    return(
+      <div>test2</div>
+    )
+  }
+}
+
+export { StyleTest2 };
--- src/containers/styleTestContainer.js
+++ src/containers/styleTestContainer.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Wrapper, SubmitButton, ResetButton, CancelButton, Title} from '../components/styleButton'
 import {StyleTest1} from '../components/styleTest1'
+import {StyleTest2} from '../components/styleTest2'
 
 class StyleComponentsTest extends Component {
   render() {
     return (
       <div>
         <StyleTest1 />
+        <StyleTest2 />
       </div>
     )
   }

0d649a1aed0145cd69157907a2f9ec08ffc939c8
github username <github email address>
2019-11-07 21:57:55 -0500

    testing Title adn Wrapper in the new container component

--- src/components/styleButton.js
+++ src/components/styleButton.js
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   background: papayawhip;
   color: white;
-`
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
 const SubmitButton = styled.button`
   color: maroon;
   font-size: 1em;
@@ -12,7 +19,7 @@ const SubmitButton = styled.button`
   borderL 2px solid;
   border-radius: 3px;
   display: inline-block;
-`
+`;
 
 const ResetButton = styled.button`
   color: tomato;
@@ -23,11 +30,11 @@ const ResetButton = styled.button`
   border: 2px solid;
   border-radius: 3px;
   display: inline-block;
-`
+`;
 
 const CancelButton = styled(SubmitButton)`
   color: black;
   border-color: black;
-`
+`;
 
-export { SubmitButton, ResetButton, Wrapper, CancelButton };
+export { SubmitButton, ResetButton, Wrapper, CancelButton, Title };
--- src/containers/styleComponentsTest.js
+++ src/containers/styleComponentsTest.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { Wrapper, SubmitButton, ResetButton, CancelButton} from '../components/styleButton'
+import { Wrapper, SubmitButton, ResetButton, CancelButton, Title} from '../components/styleButton'
 
 class StyleComponentsTest extends Component {
   render() {
     return (
 
       <Wrapper>
-        <h3>Style-Components Container</h3>
-      
+        <Title>Style-Components Container</Title>
+
         <SubmitButton>Submit</SubmitButton>
         <ResetButton>Reset</ResetButton>
         <CancelButton>Cancel</CancelButton>
